cf59754f8216ca34b9548c9c36062c44f691fbcc
Manuel Rosario <manuelosvaldorosario2329@gmail.com>
2025-11-24 16:07:58 -0400

    Update

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
+from crear_archivo import create_txt
+from add_task import add_task
+from update_task import update_task
+from reed_task import reed_task
+from delete_task import delete_task
+from close_app import close_app
+
 print("Gestor de tares")
 
 def Menu():
     try:
-        print("1-Crear Archivo\n 2-Agregar Tareas.\n3-Modificar Tareas.\n4-Leer Tareas.\n5-Eliminar Tareas.\n6-Salir de la app.")
+        print("1-Crear Archivo\n2-Agregar Tareas.\n3-Modificar Tareas.\n4-Leer Tareas.\n5-Eliminar Tareas.\n6-Salir de la app.")
         option = int(input("Ingrese una opcion:"))
 
-        """
+        
         if option == 1:
             create_txt()
+            Menu()
         elif option == 2:
             add_task()
+            Menu()
         elif option == 3:
             update_task()
+            Menu()
         elif option == 4:
-            #reed_task()
+            reed_task()
+            Menu()
         elif option == 5:
-           # delete_task()
+            delete_task()
+            Menu()
         elif option == 6:
-           # Close_app()
-        """
+            close_app()
+        
         #Manejo de errores.
-        if option > 5 or option < 1:
+        if option > 6 or option < 1:
             print("\n=============================================================================================")
-            print("Por favor ingrese un numero entre 1 y 5.")
+            print("Por favor ingrese un numero entre 1 y 6.")
             print("=============================================================================================\n")
             Menu()
 
