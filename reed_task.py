569d2d59ad87d2e34bcb2182d8a00af9ad275646
Manuel Rosario <manuelosvaldorosario2329@gmail.com>
2025-11-24 16:14:07 -0400

    Update_reed

--- reed_task.py
+++ reed_task.py
@@ -5,8 +5,12 @@ def reed_task():
         cant_task = 0
         lineas = archivo.readlines()
         if len(lineas) == 0:
+            print("\n---------------------------")
             print("No hay Tareas.")
+            print("---------------------------\n")
         else:
+            print("\n---------------------------")
             for l in lineas:
                 cant_task += 1
                 print(f"{cant_task}-{l.replace('\n', '')}")
+            print("---------------------------\n")
