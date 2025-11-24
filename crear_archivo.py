a7158e5c91e044038fe33032c4ced704339fd595
Manuel Rosario <manuelosvaldorosario2329@gmail.com>
2025-11-24 16:16:42 -0400

    Update_crear

--- crear_archivo.py
+++ crear_archivo.py
@@ -4,7 +4,11 @@ ruta = 'tareas.txt'
 
 def create_txt():
     if os.path.exists(ruta):
+        print("\n---------------------------")
         print("El archivo ya existe.")
+        print("---------------------------\n")
     else:
         open(ruta, "w").close()
+        print("\n---------------------------")
         print("Archivo Creado.")
+        print("---------------------------\n")
